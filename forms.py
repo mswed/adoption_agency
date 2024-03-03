6df19eb657c17461e5f14d704af3b38baf576428
Moshe Swed <mswed@beapot.com>
2024-03-03 16:11:57 -0500

    step 5

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, URLField
+from wtforms import StringField, IntegerField, URLField, SelectField
+from wtforms.validators import Optional, NumberRange, URL
 
 
 class PetForm(FlaskForm):
     name = StringField("Pet's name")
-    species = StringField('Species')
-    photo_url = URLField('Photo URL')
-    age = IntegerField('Age')
+    species = SelectField('Species', choices=['cat', 'dog', 'porcupine'])
+    photo_url = URLField('Photo URL', validators=[URL(message='This is not a valid URL!'), Optional()])
+    age = IntegerField('Age', validators=[NumberRange(min=0, max=30, message='Age must be between 0 and 30')])
     notes = StringField('Notes')
